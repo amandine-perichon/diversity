9fb620b14baad3854992f70b746dcaf8871703f2
EDA <eda@mac-mini-3.local>
2016-08-26 15:57:31 +1200

    Link Form to Piechat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,40 +1,23 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import DiversityForm from './DiversityForm'
 import PieChartView from './PieChartView.d3x'
 
-export default React.createClass({
-  data: [
-    {
-      age: "<5",
-      population: 9
-    },
-    {
-      age: "5-13",
-      population: 12
-    },
-    {
-      age: "14-17",
-      population: 5
-    },
-    {
-      age: "18-24",
-      population: 8
-    },
-    {
-      age: "25-44",
-      population: 5
-    },
-    {
-      age: "45-64",
-      population: 5
-    }
-  ],
+const App =  React.createClass({
   render () {
     return (
       <div>
-      <PieChartView data={this.data} />
-      <DiversityForm />
+        <DiversityForm />
+        <PieChartView data={this.props.data}/>
       </div>
     )
   }
 })
+
+function mapStateToProps (state) {
+  return {
+    data: state.ageProfile
+  }
+}
+
+export default connect(mapStateToProps)(App)
--- src/reducers/ageProfile.js
+++ src/reducers/ageProfile.js
@@ -1,18 +1,24 @@
-const initialState = {
-  "18-25": 0,
-  "26-35": 0,
-  "36-45": 0,
-  "46-55": 0,
-  "56-65": 0,
-  "65+": 0
-}
+const initialState = [
+  {age: "18-25", population: 0},
+  {age: "26-35", population: 0},
+  {age: "36-45", population: 0},
+  {age: "46-55", population: 0},
+  {age: "56-65", population: 0},
+  {age: "65+", population: 0}
+]
 
 const ageProfile = (state = initialState, action) => {
   switch (action.type) {
     case 'CHANGE_AGE_PROFILE':
-      const ageUpdate = {}
-      ageUpdate[action.age] = Number(action.population)
-      return Object.assign({}, state, ageUpdate)
+      const index = state.reduce((acc, elem, i) => {
+        if (elem.age === action.age) {
+          return i
+        } else {
+          return acc
+        }
+      }, -1)
+
+      return [...state.slice(0, index), {age: action.age, population: action.population}, ...state.slice(index+1)]
 
     default:
       return state
